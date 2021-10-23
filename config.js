21d6b8391e82fadf6069880e4d895214f599da73
Tesmi <tusharmaurya2001@gmail.com>
2021-10-23 21:46:06 +0530

    changing app password for gmail to work

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ module.exports = {
   ACCESS_TOKEN_ADMIN:
     "4196ab1a41f90e5abab0238238cc7b8c241c71d748dc42fe741f95480401a7842386a0d05c767d322f577d516e58c5cc78cbc5d08253ccc1914b3e1c971c7b26",
   email: "aglofficial29@gmail.com",
-  password: "cpbynirtwupyynek",
-   db_uri:
-     "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false",
+  password: "ybfdxbaaqhskjhwb",
+  db_uri:
+    "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false",
   //db_uri:
   //  "mongodb+srv://tushar:s2aTzU3wc7Sl3M3L@cluster0.xq4oc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
 
