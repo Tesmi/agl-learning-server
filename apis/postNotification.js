b3114eed69dcdebb0dbb000e58e15cb17eb9e848
Tesmi <tusharmaurya2001@gmail.com>
2021-11-16 13:07:45 +0530

    implementing broadcase notification serive to all users email

--- apis/postNotification.js
+++ apis/postNotification.js
@@ -1,6 +1,7 @@
+const axios = require("axios");
+
 module.exports = async (app, client) => {
   app.post("/api/postNotification", async (req, res) => {
-
     try {
       const user = req.user.name;
       const accountType = req.user.account;
@@ -31,6 +32,7 @@ module.exports = async (app, client) => {
         Board: board,
         ClassData: classData,
         Description: desc,
+        By: "",
       };
 
       dataToBeUploaded.By = user;
@@ -40,6 +42,7 @@ module.exports = async (app, client) => {
         .collection("notifications")
         .insertOne(dataToBeUploaded)
         .then((e) => {
+          broadcastNotification(dataToBeUploaded);
           return res.json({
             status: "success",
             msg: `Notification posted successfully`,
@@ -56,9 +59,73 @@ module.exports = async (app, client) => {
     } catch (error) {
       return res.json({
         status: "error",
-        msg: `Unexpected error`,
+        msg: `Server error`,
         data: {},
       });
     }
   });
+  async function broadcastNotification(data) {
+    if (data.Board == "Everyone" && data.ClassData == "Everyone") {
+      await client
+        .db("main")
+        .collection("users")
+        .find({ AccountType: "student" })
+        .toArray()
+        .then((accounts) => {
+          sendEachNotification(accounts, data);
+        });
+    } else if (data.Board == "Everyone") {
+      await client
+        .db("main")
+        .collection("users")
+        .find({ AccountType: "student", Grade: data.ClassData })
+        .toArray()
+        .then((accounts) => {
+          sendEachNotification(accounts, data);
+        });
+    } else if (data.ClassData == "Everyone") {
+      await client
+        .db("main")
+        .collection("users")
+        .find({ AccountType: "student", Grade: data.Board })
+        .toArray()
+        .then((accounts) => {
+          sendEachNotification(accounts, data);
+        });
+    } else {
+      await client
+        .db("main")
+        .collection("users")
+        .find({
+          AccountType: "student",
+          Grade: data.ClassData,
+          Board: data.Board,
+        })
+        .toArray()
+        .then((accounts) => {
+          sendEachNotification(accounts, data);
+        });
+    }
+    async function sendEachNotification(accounts, data) {
+      let msg = `<!DOCTYPE html>
+      <html lang="en">
+      <body>
+          <p><b>${data.Subject}</b> </p>
+          <p>
+              ${data.Description}
+          </p>
+          <p>From @${data.By},</p>
+          <p><b>AGL Learning App</b></p>
+      </body>
+      </html>`;
+      let sub = "Notification from AGL App";
+
+      accounts.forEach((account) => {
+        let email = account.Email;
+        axios.get("https://aglofficial.com/public/sendEmail", {
+          params: { email, msg, sub },
+        });
+      });
+    }
+  }
 };
