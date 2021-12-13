5805c821eb0fc5d9e37e0f839765cef39f7c4928
Tesmi <tusharmaurya2001@gmail.com>
2021-12-13 19:21:20 +0530

    updating video info api

--- publicApis/findVideoInfo.js
+++ publicApis/findVideoInfo.js
@@ -1,10 +1,8 @@
 module.exports = async (app, client) => {
   app.get("/public/vidInfo", async (req, res) => {
     let StreamName = req.query.name;
-    if (StreamName) StreamName = StreamName.slice(1, -1);
-
     const BaseURL = "https://jitsi.aglofficial.com/";
-
+    if (StreamName) StreamName = StreamName.split("/")[3].split("_")[0];
     const fullVidURL = BaseURL + StreamName;
 
     client
@@ -16,19 +14,58 @@ module.exports = async (app, client) => {
           client
             .db("main")
             .collection("users")
-            .findOne({})
+            .findOne({ UserName: vidData.UserName })
             .then((userData) => {
-              return res.send(
-                `_${vidData.LectureName}_${vidData.Board}_${vidData.ClassData}_${userData.FullName}`
-              );
+              let data = {
+                VideoName: Array.from(Array(30), () =>
+                  Math.floor(Math.random() * 36).toString(36)
+                ).join(""),
+                LectureName: vidData.LectureName,
+                Board: vidData.Board,
+                Class: vidData.ClassData,
+                NameOfTeacher: userData.FullName,
+              };
+
+              //push data to database
+              client
+                .db("main")
+                .collection("recordedVideos")
+                .insertOne(data)
+                .then((e) => {
+                  return res.send(data.VideoName);
+                })
+                .catch((err) => {
+                  return res.send(
+                    "INVALID_",
+                    Array.from(Array(30), () =>
+                      Math.floor(Math.random() * 36).toString(36)
+                    ).join("")
+                  );
+                });
             })
             .catch((err) => {
-              return res.send("_N/A_Everyone_Everyone_N/A");
+              return res.send(
+                "INVALID_",
+                Array.from(Array(30), () =>
+                  Math.floor(Math.random() * 36).toString(36)
+                ).join("")
+              );
             });
-        } else return res.send("_N/A_Everyone_Everyone_N/A");
+        } else
+          return res.send(
+            "INVALID_",
+            Array.from(Array(30), () =>
+              Math.floor(Math.random() * 36).toString(36)
+            ).join("")
+          );
       })
       .catch((err) => {
-        return res.send("_N/A_Everyone_Everyone_N/A");
+        return res.send(
+          "INVALID_",
+          Array.from(Array(30), () =>
+            Math.floor(Math.random() * 36).toString(36)
+          ).join("")
+        );
       });
   });
 };
